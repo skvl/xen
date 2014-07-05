abaa4d0198174178deaa15a5be666a28a3406354
Bastian Blank <waldi@debian.org>
2014-07-05 11:46:56 +0200

    tools-libfsimage-rpath.diff

--- tools/libfsimage/Rules.mk
+++ tools/libfsimage/Rules.mk
@@ -3,6 +3,7 @@ include $(XEN_ROOT)/tools/Rules.mk
 CFLAGS += -Wno-unknown-pragmas -I$(XEN_ROOT)/tools/libfsimage/common/ -DFSIMAGE_FSDIR=\"$(FSDIR)\"
 CFLAGS += -Werror -D_GNU_SOURCE
 LDFLAGS += -L../common/
+LDFLAGS += $(call LDFLAGS_RPATH,../..)
 
 PIC_OBJS := $(patsubst %.c,%.opic,$(LIB_SRCS-y))
 
